1d6edcc5920514ab226ac905f5f3daeb07142b94
AghastyGD <augustodomingosvasco@gmail.com>
2023-11-29 05:09:21 -0200

    add_interface_user_repository

--- src/main/java/vercel/app/augustodomingos/todolist/user/UserController.java
+++ src/main/java/vercel/app/augustodomingos/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package vercel.app.augustodomingos.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) Numeros 0.00000
-     * Float (float) Numeros 0.000
-     * Char (A C)
-     * Date (data)
-     * void
-     */
-    /* Body */ 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            // Mensagem de Erro
+            // Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
