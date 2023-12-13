8f93d6ceb03bf4acc7473b3e6139d085065c98a8
AghastyGD <augustodomingosvasco@gmail.com>
2023-12-13 05:12:51 -0200

    feature: listar tarefas

--- src/main/java/vercel/app/augustodomingos/todolist/task/ITaskRepository.java
+++ src/main/java/vercel/app/augustodomingos/todolist/task/ITaskRepository.java
@@ -1,9 +1,10 @@
 package vercel.app.augustodomingos.todolist.task;
 
 import java.util.UUID;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    
+    List<TaskModel> findByIdUser(UUID idUser);
 }
