75d4d21e71a1879704412cc6f67f9e1084ac6aff
AghastyGD <augustodomingosvasco@gmail.com>
2023-12-13 06:24:30 -0200

    feature: parcel update

--- src/main/java/vercel/app/augustodomingos/todolist/task/TaskController.java
+++ src/main/java/vercel/app/augustodomingos/todolist/task/TaskController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import vercel.app.augustodomingos.todolist.utils.Utils;
 
 @RestController
 @RequestMapping("/tasks")
@@ -55,10 +56,12 @@ public class TaskController {
     // http://localhost:8080/tasks/09090192-cdfsfs-893443
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
 
     }
     
